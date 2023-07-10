0e25c5ebd546fb634fd8f54ff02f472ee5ef5b98
Julian Fenison <julian.fenison@gmail.com>
2023-07-10 16:43:12 -0500

    Working through the exercises today.

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,9 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+
+document.querySelector(".page-header__heading").style.color = "#154734";
+document.querySelectorAll(".feature").forEach(item => item.style.backgroundColor = "#154734");
+document.querySelectorAll(".feature").forEach(item => item.style.padding = "10px");
+document.querySelectorAll(".feature").forEach(item => item.style.color = "white");
--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,11 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+
+document.querySelector(".backpack__volume").classList.add("new-class1");
+document.querySelector(".backpack__volume").classList.add("new-class2");
+document.querySelector(".backpack__volume").classList.remove("new-class1");
+
+document.querySelector(".leftlength input").setAttribute("placeholder", "julian");
+document.querySelector(".lid-toggle").style.color = "red";
