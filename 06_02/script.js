0bca51333aeb9b9f28b75f67d25690ff34fc0011
Julian Fenison <julian.fenison@gmail.com>
2023-07-16 21:26:25 -0500

    Working through chapter 6 courses

--- 06_02/script.js
+++ 06_02/script.js
@@ -2,3 +2,9 @@
  * The var statement
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
  */
+
+var container = 5;
+
+container = "red";
+
+console.log(container);
