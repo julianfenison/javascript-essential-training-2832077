8e7251cffc9e4c0bbb926163b379deafb19f9b33
Julian Fenison <julian.fenison@gmail.com>
2023-07-27 16:52:17 -0500

    Working on exercise

--- Practice/08_09/script.js
+++ Practice/08_09/script.js
@@ -34,26 +34,71 @@ const frogpack = {
 };
 
 // Baseline HTML output
-const content = `
-    <h1 class="backpack__name">${frogpack.name}</h1>
-    <ul class="backpack__features">
-      <li class="packprop backpack__volume">Volume:<span> ${
-        frogpack.volume
-      }l</span></li>
-      <li class="packprop backpack__color">Color:<span> ${
-        frogpack.color
-      }</span></li>
-      <li class="packprop backpack__pockets">Number of pockets:<span> ${
-        frogpack.pocketNum
-      }</span></li>
-      <li class="packprop backpack__strap">Left strap length:<span> ${
-        frogpack.strapLength.left
-      } inches</span></li>
-      <li class="packprop backpack__strap">Right strap length:<span> ${
-        frogpack.strapLength.right
-      } inches</span></li>
-      <li class="feature backpack__lid">Lid status:<span> ${
-        frogpack.lidOpen ? "open" : "closed"
-      }</span></li>
-    </ul>  
+// const content = `
+//     <h1 class="backpack__name">${frogpack.name}</h1>
+//     <ul class="backpack__features">
+//       <li class="packprop backpack__volume">Volume:<span> ${
+//         frogpack.volume
+//       }l</span></li>
+//       <li class="packprop backpack__color">Color:<span> ${
+//         frogpack.color
+//       }</span></li>
+//       <li class="packprop backpack__pockets">Number of pockets:<span> ${
+//         frogpack.pocketNum
+//       }</span></li>
+//       <li class="packprop backpack__strap">Left strap length:<span> ${
+//         frogpack.strapLength.left
+//       } inches</span></li>
+//       <li class="packprop backpack__strap">Right strap length:<span> ${
+//         frogpack.strapLength.right
+//       } inches</span></li>
+//       <li class="feature backpack__lid">Lid status:<span> ${
+//         frogpack.lidOpen ? "open" : "closed"
+//       }</span></li>
+//     </ul>  
+// `;
+
+// const addImage = (currentPack) => {
+//   const newFigure = document.createElement("figure");
+//   return newFigure; 
+// };
+
+const addFigure = (currentPack) => {
+  const newImage = new Image();
+  newImage.src = currentPack.image;
+  const newFigure = document.createElement("figure");
+  newFigure.innerHTML = `
+  `;
+};
+
+const addPack = (currentPack) => {
+  const newArticle = document.createElement("article");
+  newArticle.innerHTML = `<h1 class="backpack__name">${currentPack.name}</h1>
+  <ul class="backpack__features">
+    <li class="packprop backpack__volume">Volume:<span> ${
+      currentPack.volume
+    }l</span></li>
+    <li class="packprop backpack__color">Color:<span> ${
+      currentPack.color
+    }</span></li>
+    <li class="packprop backpack__pockets">Number of pockets:<span> ${
+      currentPack.pocketNum
+    }</span></li>
+    <li class="packprop backpack__strap">Left strap length:<span> ${
+      currentPack.strapLength.left
+    } inches</span></li>
+    <li class="packprop backpack__strap">Right strap length:<span> ${
+      currentPack.strapLength.right
+    } inches</span></li>
+    <li class="feature backpack__lid">Lid status:<span> ${
+      currentPack.lidOpen ? "open" : "closed"
+    }</span></li>
+  </ul>  
 `;
+
+  return newArticle;
+};
+
+const main = document.querySelector("main");
+main.append(addPack(frogpack));
+
